03870206ad2d67636f932c2aa9ed4fab4c29f167
Ohshima Yusuke (s-giken) <ooshima@s-giken.com>
2025-05-20 17:08:17 +0900

    過去に作成した回答案をマージ

---
+++ webapp/src/main/java/com/s_giken/training/webapp/model/MemberSearchCondition.java
@@ -0,0 +1,16 @@
+package com.s_giken.training.webapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // メンバー変数に対するゲッター・セッターを自動生成
+@NoArgsConstructor // 引数のないコンストラクタを自動生成
+@AllArgsConstructor // 全てのメンバ変数に対する引数を持つコンストラクタを自動生成
+public class MemberSearchCondition {
+    // メールアドレス検索用
+    private String mail;
+    private String name;
+    private String sortColName;
+    private String sortOrder;
+}
