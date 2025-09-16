b55dc85fedbd6bbef585e3c90022b0f573e98285
Ohshima Yusuke (s-giken) <ooshima@s-giken.com>
2025-09-16 13:57:21 +0900

    不足していたテーブル定義を追加

--- webapp/src/main/resources/schema.sql
+++ webapp/src/main/resources/schema.sql
@@ -1,42 +1,74 @@
-CREATE TABLE IF NOT EXISTS T_BILLING_STATUS (
-    billing_ym DATE PRIMARY KEY,
-    is_commit BOOLEAN NOT NULL DEFAULT FALSE
-);
+CREATE TABLE
+    IF NOT EXISTS T_MEMBER (
+        MEMBER_ID BIGINT NOT NULL,
+        MAIL CHARACTER VARYING(255) NOT NULL,
+        NAME CHARACTER VARYING(31) NOT NULL,
+        ADDRESS CHARACTER VARYING(127) NOT NULL,
+        START_DATE DATE NOT NULL,
+        END_DATE DATE,
+        PAYMENT_METHOD TINYINT NOT NULL,
+        CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        MODIFIED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (MEMBER_ID)
+    );
 
+CREATE TABLE
+    IF NOT EXISTS T_CHARGE (
+        CHARGE_ID BIGINT NOT NULL,
+        NAME CHARACTER VARYING(127) NOT NULL,
+        AMOUNT NUMERIC(9, 0) NOT NULL,
+        START_DATE DATE NOT NULL,
+        END_DATE DATE,
+        CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        MODIFIED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (CHARGE_ID)
+    );
 
-CREATE TABLE IF NOT EXISTS T_BILLING_DATA (
-    billing_ym DATE NOT NULL,
-    member_id INTEGER NOT NULL,
-    mail VARCHAR(255) NOT nULL,
-    name VARCHAR(31) NOT NULL,
-    address VARCHAR(127) NOT nULL,
-    start_date DATE NOT NULL,
-    end_date DATE,    
-    payment_method INTEGER NOT NULL,
-    amount NUMBER(10,0) NOT NULL,
-    tax_ratio NUMBER(5,2) NOT NULL,
-    total NUMBER(10,0) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE
+    IF NOT EXISTS T_BILLING_STATUS (
+        billing_ym DATE PRIMARY KEY,
+        is_commit BOOLEAN NOT NULL DEFAULT FALSE
+    );
 
-    PRIMARY KEY (billing_ym, member_id),
-    FOREIGN KEY (billing_ym) 
-        REFERENCES T_BILLING_STATUS(billing_ym)
-);
+CREATE TABLE
+    IF NOT EXISTS T_BILLING_DATA (
+        billing_ym DATE NOT NULL,
+        member_id INTEGER NOT NULL,
+        mail VARCHAR(255) NOT nULL,
+        name VARCHAR(31) NOT NULL,
+        address VARCHAR(127) NOT nULL,
+        start_date DATE NOT NULL,
+        end_date DATE,
+        payment_method INTEGER NOT NULL,
+        amount NUMBER (10, 0) NOT NULL,
+        tax_ratio NUMBER (5, 2) NOT NULL,
+        total NUMBER (10, 0) NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (billing_ym, member_id),
+        FOREIGN KEY (billing_ym) REFERENCES T_BILLING_STATUS (billing_ym)
+    );
 
+CREATE TABLE
+    IF NOT EXISTS T_BILLING_DETAIL_DATA (
+        billing_ym DATE NOT NULL,
+        member_id INTEGER NOT NULL,
+        charge_id INTEGER NOT NULL,
+        name VARCHAR(63) NOT NULL,
+        amount NUMBER (9, 0) NOT NULL,
+        start_date DATE NOT NULL,
+        end_date DATE,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (billing_ym, member_id, charge_id),
+        FOREIGN KEY (billing_ym, member_id) REFERENCES T_BILLING_DATA (billing_ym, member_id)
+    );
 
-CREATE TABLE IF NOT EXISTS T_BILLING_DETAIL_DATA (
-    billing_ym DATE NOT NULL,
-    member_id INTEGER NOT NULL,
-    charge_id INTEGER NOT NULL,
-    name VARCHAR(63) NOT NULL,
-    amount NUMBER(9,0) NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    PRIMARY KEY (billing_ym, member_id, charge_id),
-    FOREIGN KEY (billing_ym, member_id) 
-        REFERENCES T_BILLING_DATA(billing_ym, member_id)
-);
+CREATE TABLE
+    IF NOT EXISTS T_ACCOUNT (
+        username VARCHAR(127) PRIMARY KEY,
+        password VARCHAR(512) NOT NULL,
+        role VARCHAR(64) NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
